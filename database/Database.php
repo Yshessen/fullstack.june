87724c5a563200e73b00cb88dcf661309c044e61
Jorn <2108293@talnet.nl>
2022-06-23 14:42:42 +0200

    inloggen update
    not finished yet

---
+++ database/Database.php
@@ -0,0 +1,12 @@
+<?php 
+  $db_host = "localhost";
+  $db_name = "id19138934_animelistodb";
+  $db_pass = "id19138934_animan";
+  $db_user = "iFcFBS!4^rTbm{7Y";
+ 
+  $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+ 
+  if (!$conn){
+    die ('Failed to connect with server');
+  }   
+?>
